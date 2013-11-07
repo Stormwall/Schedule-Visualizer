3cc41cff98f7a1f9416fbe43cae7cb98bd363fee
Stormwall <erikhaus.home@googlemail.com>
2013-11-07 12:53:04 +0100

    First Push

---
+++ src/sv/creation/adress/MainApplication.java
@@ -0,0 +1,16 @@
+package sv.creation.adress;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
