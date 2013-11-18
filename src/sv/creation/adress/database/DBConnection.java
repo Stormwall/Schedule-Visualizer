ddc492e44ab2942ac1e5da26ab76986292c76628
DennisT88 <dennis.than703@gmail.com>
2013-11-18 13:28:25 +0100

    DBCONNECTION updated

--- src/sv/creation/adress/database/DBConnection.java
+++ src/sv/creation/adress/database/DBConnection.java
@@ -70,9 +70,19 @@ public class DBConnection {
 		try{
 			Statement stmnt=connection.createStatement();
 			//Checks if table exists or not
-			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Diensttyp (DutyID int NOT NULL PRIMARY KEY, DutyType VARCHAR(50) NOT NULL);");
-			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Dienst (DutyID int NOT NULL PRIMARY KEY, BlockID int NOT NULL PRIMARY KEY, ServiceJourneyID int NOT NULL, FromStopID int NOT NULL, ToStopID int NOT NULL, DepTime date NOT NULL, ArrTime date NOT NULL, ElementType int NOT NULL, ServiceJourneyCode int);");
-			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Umlauf (DutyID int NOT NULL PRIMARY KEY, DutyType VARCHAR(50));");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Diensttyp (DutyID INTEGER NOT NULL PRIMARY KEY, DutyType VARCHAR(50) NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Dienst (BlockID INTEGER NOT NULL PRIMARY KEY, ServiceJourneyID INTEGER NOT NULL, FromStopID INTEGER NOT NULL, ToStopID INTEGER NOT NULL, DepTime date NOT NULL, ArrTime date NOT NULL, ElementType INTEGER NOT NULL, ServiceJourneyCode INTEGER);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Umlauf (BlockID INTEGER NOT NULL PRIMARY KEY, DutyType VARCHAR(50));");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS FahrtZuUmlauf (ID INTEGER PRIMARY KEY AUTOINCREMENT, BlockID INTEGER NOT NULL, ServiceJourneyID INTEGER NOT NULL, FromStopID INTEGER NOT NULL, ToStopID INTEGER NOT NULL, DepTime VARCHAR(10) NOT NULL, ArrTime VARCHAR(10) NOT NULL, ElementType INTEGER NOT NULL, ServiceJourneyCode INTEGER);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Haltestellen (ID INTEGER NOT NULL PRIMARY KEY, Code INTEGER NOT NULL, Name INTEGER NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Linien (ID INTEGER NOT NULL PRIMARY KEY, Code INTEGER NOT NULL, Name INTEGER NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Fahrzeugtypen (ID INTEGER NOT NULL PRIMARY KEY, Code INTEGER NOT NULL, Name INTEGER NOT NULL, VehCost INTEGER NOT NULL, KmCost INTEGER NOT NULL, HourCost INTEGER NOT NULL, Capacity INTEGER NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Fahrzeugtypgruppen (ID INTEGER NOT NULL PRIMARY KEY, Code INTEGER NOT NULL, Name INTEGER NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS FahrzeugtypZuFahrzeugtypgruppe (ID INTEGER PRIMARY KEY AUTOINCREMENT, VehTypeID INTEGER NOT NULL, VehTypeGroupID INTEGER NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS FahrzeugKapazitaetenZuHaltestelle (ID INTEGER PRIMARY KEY AUTOINCREMENT, VehTypeID INTEGER NOT NULL, StoppointID INTEGER NOT NULL, Min INTEGER NOT NULL, Max INTEGER NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Linienfahrten (ID INTEGER NOT NULL PRIMARY KEY, LineID INTEGER NOT NULL, FromStopID INTEGER NOT NULL, ToStopID INTEGER NOT NULL, DepTime VARCHAR(10) NOT NULL, ArrTime VARCHAR(10) NOT NULL, MinAheadTime INTEGER NOT NULL, MinLayoverTime INTEGER NOT NULL, VehTypeGroupID INTEGER NOT NULL, MaxShiftBackwardSeconds INTEGER NOT NULL, MaxShiftForwardSeconds INTEGER NOT NULL, FromStopBreakFacility INTEGER NOT NULL, ToStopBreakFacility INTEGER NOT NULL, Code INTEGER);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Verbindungen (ID INTEGER PRIMARY KEY AUTOINCREMENT, FromStopID INTEGER NOT NULL, ToStopID INTEGER NOT NULL, FromTime VARCHAR(10) NOT NULL, ToTime VARCHAR(10) NOT NULL, Distance INTEGER NOT NULL, RunTime INTEGER NOT NULL);");
+			stmnt.executeUpdate("CREATE TABLE IF NOT EXISTS Abloesepunkte (ID INTEGER NOT NULL PRIMARY KEY, ServiceJourneyID INTEGER NOT NULL, StoppointID INTEGER NOT NULL, StopTime VARCHAR(10) NOT NULL);");
 			
 		}catch(SQLException e){
 			System.out.println("Could not execute SQL-Query!");
