ff42bd944fd70ebbc76a0e1074ecfb4d360ffbd8
DennisT88 <dennis.than703@gmail.com>
2013-12-06 19:19:30 +0100

    DBMatching problem behoben!

--- src/sv/creation/adress/database/DBMatching.java
+++ src/sv/creation/adress/database/DBMatching.java
@@ -16,132 +16,169 @@ import sv.creation.adress.model.Umlaufplan;
  */
 
 public class DBMatching {
+	 
+    // **********************************************************************
+    // ****** Array lists to create objects of the schedule elements  *******
+    // ******                                                         *******
+    // **********************************************************************
+   
 
-	// **********************************************************************
-	// ****** Array lists to create objects of the schedule elements  *******
-	// ******                                              			  *******
-	// **********************************************************************
-	
+   
+    ArrayList<Block> umlauf = new ArrayList<Block>();
+    ArrayList<Blockelement> fahrtZuUmlauf = new ArrayList<Blockelement>();
 
-	
-	ArrayList<Block> umlauf = new ArrayList<Block>();
-	ArrayList<Blockelement> fahrtZuUmlauf = new ArrayList<Blockelement>();
+    //Object of a database statement
+    Statement stmt;
+    Statement stmt2;
+    Statement stmt3;
 
-	//Object of a database statement
-	Statement stmt;
+    // **********************************************************************
+    // ****** In this method an block object will be created          *******
+    // ******                                                         *******
+    // **********************************************************************
+   
+    public void erstelleUmlaufplan() {
 
-	// **********************************************************************
-	// ****** In this method an block object will be created          *******
-	// ******                                              			  *******
-	// **********************************************************************
-	
-	public void erstelleUmlaufplan() {
+          DBConnection db = new DBConnection();
+          db.initDBConnection();
 
-		DBConnection db = new DBConnection();
-		db.initDBConnection();
+          //Creating a sql query
+          try {
+                 stmt = db.getConnection().createStatement();
 
-		//Creating a sql query 
-		try {
-			stmt = db.getConnection().createStatement();
+                 ResultSet rest1 = stmt
+                               .executeQuery("SELECT  DISTINCT * FROM Block");
 
-			ResultSet rest1 = stmt
-					.executeQuery("SELECT  DISTINCT * FROM Block");
+                 //All resulted datasets of the sql query will be added to the block array list
+                 while (rest1.next()) {
+                        int id = Integer.parseInt(rest1.getString("BlockID"));
+                        int code = Integer.parseInt(rest1.getString("Code"));
+                        String name = rest1.getString("Name");
+                        umlauf.add(new Block(id, code, name));
+                 }
+          } catch (SQLException e) {
+                 // TODO Auto-generated catch block
+                 e.printStackTrace();
+          }
+    }
 
-			//All resulted datasets of the sql query will be added to the block array list 
-			while (rest1.next()) {
-				int id = Integer.parseInt(rest1.getString("BlockID"));
-				int code = Integer.parseInt(rest1.getString("Code"));
-				String name = rest1.getString("Name");
-				umlauf.add(new Block(id, code, name));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    // **********************************************************************
+    // ****** In this method an blockelement object will be created   *******
+    // ******                                                         *******
+    // **********************************************************************
+   
+    public void test() {
+         
+          DBConnection db = new DBConnection();
+          db.initDBConnection();
 
-	// **********************************************************************
-	// ****** In this method an blockelement object will be created   *******
-	// ******                                              			  *******
-	// **********************************************************************
-	
-	public void test() {
-		
-		DBConnection db = new DBConnection();
-		db.initDBConnection();
+          try {
+        	  
+        	  //Creating separated statements for the sql queries
+                 stmt = db.getConnection().createStatement();
+                 stmt2=db.getConnection().createStatement();
+                 stmt3=db.getConnection().createStatement();
 
-		try {
-			stmt = db.getConnection().createStatement();
-			
-			ResultSet rest3=stmt.executeQuery("SELECT * FROM Blockelement;");
-			//rest3.first();
-			//All resulted datasets of the sql query will be added to the blockelement array list 
-			while (rest3.next()) {
-//				ResultSet rest4=stmt.executeQuery("SELECT elementType FROM Blockelement;");
-				System.out.println(rest3.getString("ElementType"));
-				if(rest3.getString("ElementType").equals("1")){
-					
-					ResultSet rest2 = stmt
-							.executeQuery("SELECT be.ID, be.BlockID, be.ServiceJourneyID, sj.FromStopID, sj.ToStopID, sj.DepTime, sj.ArrTime, be.elementType FROM Blockelement AS be, ServiceJourney AS sj WHERE sj.serviceJourneyID='"+rest3.getString("ServiceJourneyID")+"';");
-						int id = Integer.parseInt(rest2.getString("ID"));
-						int blockID = Integer.parseInt(rest2.getString("BlockID"));
-						String serviceJourneyID = rest2
-								.getString("ServiceJourneyID");
-						int fromStopID = Integer
-								.parseInt(rest2.getString("FromStopID"));
-						int toStopID = Integer.parseInt(rest2.getString("ToStopID"));
-						String depTime = rest2.getString("DepTime");
-						String arrTime = rest2.getString("ArrTime");
-						int elementType = Integer.parseInt(rest2
-								.getString("ElementType"));
+                 //Two different datasets will be created
+                 ResultSet rest3 = stmt.executeQuery("SELECT * FROM Blockelement;");
+                 ResultSet rest2;
 
-						fahrtZuUmlauf.add(new Blockelement(id, blockID,
-								serviceJourneyID, fromStopID, toStopID, depTime,
-								arrTime, elementType));
+                 // All resulted datasets of the sql query will be added to the
+                 // blockelement array list
 
-					
-				}else{ ResultSet rest2=stmt.executeQuery("SELECT be.ID, be.BlockID, be.ServiceJourneyID, ex.FromStopID, ex.ToStopID, ex.DepTime, ex.ArrTime, be.ElementType FROM Blockelement AS be, ExceptionalBlockelement AS ex WHERE ex.ServiceJourneyID='"+rest3.getString("ServiceJourneyID")+"';");
-				int id = Integer.parseInt(rest2.getString("ID"));
-				int blockID = Integer.parseInt(rest2.getString("BlockID"));
-				String serviceJourneyID = rest2
-						.getString("ServiceJourneyID");
-				int fromStopID = Integer
-						.parseInt(rest2.getString("FromStopID"));
-				int toStopID = Integer.parseInt(rest2.getString("ToStopID"));
-				String depTime = rest2.getString("DepTime");
-				String arrTime = rest2.getString("ArrTime");
-				int elementType = Integer.parseInt(rest2
-						.getString("ElementType"));
+                 while (rest3.next()) {
+                        System.out.println(rest3.getString("ElementType"));
+                        int zahl=Integer.parseInt(rest3.getString("ElementType"));
+                        
+                        //if the blockelement is a service journey the first sql query will be execute
+                        if (zahl==1) {
 
-				fahrtZuUmlauf.add(new Blockelement(id, blockID,
-						serviceJourneyID, fromStopID, toStopID, depTime,
-						arrTime, elementType));
-				//rest2.close();
-				}
-				System.out.println("Hat geklappt!");
-				System.out
-				.println(fahrtZuUmlauf.size());
-			}
-			
+                               rest2 = stmt2
+                                            .executeQuery("SELECT be.ID, be.BlockID, be.ServiceJourneyID, sj.FromStopID, sj.ToStopID, sj.DepTime, sj.ArrTime, be.elementType FROM Blockelement AS be, ServiceJourney AS sj WHERE sj.serviceJourneyID='"
+                                                         + rest3.getString("ServiceJourneyID")
+                                                         + "';");
+                        //if the read blockelement is a exceptional servie journey the second sql query will be execute
+                        } else {
+                               rest2 = stmt3
+                                            .executeQuery("SELECT be.ID, be.BlockID, be.ServiceJourneyID, ex.FromStopID, ex.ToStopID, ex.DepTime, ex.ArrTime, be.ElementType FROM Blockelement AS be, ExceptionalBlockelement AS ex WHERE ex.ServiceJourneyID='"
+                                                          + rest3.getString("ServiceJourneyID")
+                                                         + "';");
+                        }
+                       
+                        //the attributes will be read and save in variables
+                        int id = Integer.parseInt(rest2.getString("ID"));
+                        int blockID = Integer.parseInt(rest2.getString("BlockID"));
+                        String serviceJourneyID = rest3.getString("ServiceJourneyID");
+                        System.out.println(serviceJourneyID);
+                        int fromStopID = Integer
+                                     .parseInt(rest2.getString("FromStopID"));
+                        int toStopID = Integer.parseInt(rest2.getString("ToStopID"));
+                        String depTime = rest2.getString("DepTime");
+                        String arrTime = rest2.getString("ArrTime");
+                        int elementType = Integer.parseInt(rest2
+                                     .getString("ElementType"));
+                
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+                        //all variables will be sum up to an umlaufelement
+                        fahrtZuUmlauf.add(new Blockelement(id, blockID,
+                                     serviceJourneyID, fromStopID, toStopID, depTime,
+                                     arrTime, elementType));
+                
+                        System.out.println("Hat geklappt!");
+                        System.out.println(fahrtZuUmlauf.size());
+                 }
+          } catch (SQLException e) {
+                 // TODO Auto-generated catch block
+                 e.printStackTrace();
+          }
 
-	}
+    }
 
-	// ***********************************************************************************************
-	// ****** In this method both array lists will be integrated in an umlaufplan object.      *******
-	// ****** This method returns an completely object of a umlaufplan, which can be modified. *******                                             			  *******
-	// ***********************************************************************************************
-	
-	public void erstelleUmlaufplanDaten() {
-		Umlaufplan umlaufplan = new Umlaufplan(1, umlauf, fahrtZuUmlauf);
-		System.out
-				.println(umlaufplan.getFahrtZuUmlauf().get(0).getArrTime());
-		System.out
-		.println(umlaufplan.getFahrtZuUmlauf().size());
-	}
+    // ***********************************************************************************************
+    // ****** In this method both array lists will be integrated in an umlaufplan object.      *******
+    // ****** This method returns an completely object of a umlaufplan, which can be modified. *******                                                                   *******
+    // ***********************************************************************************************
+   
+    public void erstelleUmlaufplanDaten() {
+          Umlaufplan umlaufplan = new Umlaufplan(1, umlauf, fahrtZuUmlauf);
+          
+          //Testing the outputs
+          System.out
+                        .println(umlaufplan.getFahrtZuUmlauf().get(0).getArrTime());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(0).getArrTime());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(5).getBlockID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(7).getArrTime());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(12).getDepTime());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(0).getArrTime());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(17).getServiceJourneyID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(20).getServiceJourneyID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(1).getServiceJourneyID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(2).getServiceJourneyID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(17).getServiceJourneyID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(20).getServiceJourneyID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(2).getArrTime());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(14).getBlockID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(1).getFromStopID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(24).getArrTime());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().get(1897).getServiceJourneyID());
+          System.out
+          .println(umlaufplan.getFahrtZuUmlauf().size());
+    }
 
 }
